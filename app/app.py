e146aa2a5d1740350e8575cc35696c721a326de2
HiPeople21 <86291833+HiPeople21@users.noreply.github.com>
2021-08-17 19:13:44 +0000

    Added styling to news items and a navbar

--- app/app.py
+++ app/app.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template
 from utils import scrape_health_news
-from typing import List
+from typing import List, Dict
 
 app: Flask = Flask(__name__, template_folder='templates')
 
-
+@app.route('/home')
 @app.route('/')
 def home() ->str:
-  news: List[str] = scrape_health_news()
   
-  return render_template('index.html', content={'news': news})
+  return render_template('index.html')
+
+@app.route('/news')
+def news() ->str:
+  news: List[Dict[str, str]] = scrape_health_news()
+  
+  return render_template('news.html', content={'news': news})
 
 
 if __name__ == "__main__":
--- app/utils.py
+++ app/utils.py
@@ -1,35 +1,43 @@
 from bs4 import BeautifulSoup
 import requests
-from typing import List
+from typing import List, Dict
 
-def scrape_health_news() -> List[str]:
+def scrape_health_news() -> List[Dict[str, str]]:
   '''
   Returns a dict of news items; the key being the contents and the value being the link
   '''
-  header: dict = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
+  header: Dict[str, str] = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 
   html_text: str = requests.get('https://www.google.com/search?q=health+ireland&tbm=nws', headers=header).text
   soup: str = BeautifulSoup(html_text, 'lxml')
 
   news: List[str] = soup.find_all('g-card', class_='nChh6e')
-  # for new in news:
-  #   imgs: str = new.find_all('div', class_='vC5xic')
-  #   for img in imgs:
-  #     img.replaceWith('')
-  # for new in news:
-  #   imgs: str = new.find_all('g-img', class_='vC5xic')
-  #   for img in imgs:
-  #     img.replaceWith('')
-  # print(news)
+  # url: start=0 first google page, start=10 second google page ect
 
-
-  links = []
+  publishers: List[str] = []
+  titles: List[str] = []
+  links: List[str] = []
+  times: List[str] = []
+  main_text: List[str] = []
   for i in news:
     new_soup = BeautifulSoup(str(i), 'lxml')
     links.append(new_soup.find('a').get('href'))
+  for i in news:
+    new_soup = BeautifulSoup(str(i), 'lxml')
+    titles.append(new_soup.find('div', class_='JheGif nDgy9d').text)
+  for i in news:
+    new_soup = BeautifulSoup(str(i), 'lxml')
+    publishers.append(new_soup.find('div', class_='XTjFC WF4CUc').text)
+  for i in news:
+    new_soup = BeautifulSoup(str(i), 'lxml')
+    times.append(new_soup.find('span', class_='WG9SHc').text)
+  for i in news:
+    new_soup = BeautifulSoup(str(i), 'lxml')
+    main_text.append(new_soup.find('div', class_='Y3v8qd').text)
+  
 
-  news = {i:j for i in news for j in links}
-  return news
+  filtered_news: List[Dict[str, str]] = [{'publisher': publishers[i], 'title': titles[i], 'link': links[i], 'time': times[i], 'text': main_text[i]} for i in range(len(times))]
+  return filtered_news
 
 # print(scrape_health_news())
 scrape_health_news()
